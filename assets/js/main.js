98e3123425d468759b2bc3918dbd2814704f9c67
doannguyen263 <doannguyen263@gmail.com>
2021-11-05 10:46:46 +0700

    Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 $(function() {
 
 	new WOW().init();
-
+	if($('body').hasClass('home')){
+		$('.home-story-slider').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: false,
+			easing: "linear"
+		});
+	}
 });
