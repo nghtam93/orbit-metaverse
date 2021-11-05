9f99607702affb9e76fdf902aba0f1864589b934
doannguyen263 <doannguyen263@gmail.com>
2021-11-05 17:20:32 +0700

    Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
+	var header_sticky=$("header.-fix")
+    if($('body').hasClass( "home" )){
+        $(window).scroll(function(){
+            $(this).scrollTop()>1?header_sticky.addClass("is-active"):header_sticky.removeClass("is-active")
+        })
+    }else{
+        header_sticky.addClass("is-active")
+    }
+
 	new WOW().init();
 	if($('body').hasClass('home')){
 		$('.home-story-slider').slick({
@@ -10,4 +19,11 @@ $(function() {
 			easing: "linear"
 		});
 	}
+
+	var back_to_top=$(".scrollTop"),offset=220,duration=200;$(window).scroll(function(){
+		$(this).scrollTop()>offset?back_to_top.addClass("active"):back_to_top.removeClass("active")}),
+	$(document).on("click",".scrollTop",function(o){
+		return o.preventDefault(),$("html, body").animate({scrollTop:0},duration),!1
+	});
+
 });
